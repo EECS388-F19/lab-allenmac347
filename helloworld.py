82c396bc41bbcf21909a6c176da13161a0e33610
allenmac347 <amaca@umich.edu>
2019-09-09 14:37:22 -0400

    test script

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import random
+
+print("Allen Macaspac")
+randOne = random.randint(0, 100)
+randTwo = random.randint(0, 100)
+print(randOne)
+print(randTwo)
+print("Sum =", randOne + randTwo)
+print("Average =", (randOne + randTwo) / 2)
